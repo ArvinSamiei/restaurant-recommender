95a9f0de68eb25efe8321933b6752a28db6e4107
ArvinSamiei <samiei.arvin@gmail.com>
2025-03-24 21:20:34 -0400

    support development and production environment in frontend Dockerfile

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
+const baseURL = import.meta.env.VITE_API_URL;
+
 const API = axios.create({
-  baseURL: "http://localhost:8000",
+  baseURL,
 });
 
 API.interceptors.request.use((config) => {
